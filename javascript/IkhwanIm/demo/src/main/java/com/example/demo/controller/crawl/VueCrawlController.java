b13b26e10b8474dfb57d831cd392808af0ad4471
ekank <ekankr2@naver.com>
2021-08-09 16:07:04 +0900

    JPA 기반의 회원 가입 암호화

--- javascript/IkhwanIm/demo/src/main/java/com/example/demo/controller/crawl/VueCrawlController.java
+++ javascript/IkhwanIm/demo/src/main/java/com/example/demo/controller/crawl/VueCrawlController.java
@@ -4,22 +4,20 @@ import com.example.demo.entity.DaumNews;
 import com.example.demo.service.VueDaumNewsCrawlService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
 @RestController
+@RequestMapping("/daumCrawler")
 @CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
 public class VueCrawlController {
 
     @Autowired
     VueDaumNewsCrawlService service;
 
-    @GetMapping("{category}")
+    @GetMapping("'{category}'")
     public List<DaumNews> getDaumNewsCategory(@PathVariable String category) {
         log.info("Crawling with getDaumNewsCategory(): " + category);
 
