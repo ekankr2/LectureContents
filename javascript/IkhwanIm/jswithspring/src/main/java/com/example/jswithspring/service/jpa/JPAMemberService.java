eb4687dd4012c48ebc1ac774995edc2a23f5a194
ekank <ekankr2@naver.com>
2021-08-12 19:45:45 +0900

    jswithspring cloned

---
+++ javascript/IkhwanIm/jswithspring/src/main/java/com/example/jswithspring/service/jpa/JPAMemberService.java
@@ -0,0 +1,19 @@
+package com.example.jswithspring.service.jpa;
+
+import com.example.jswithspring.controller.vue.jpa.request.MemberRequest;
+import com.example.jswithspring.entity.jpa.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JPAMemberService {
+    public void register(MemberRequest memberRequest) throws Exception;
+
+    public boolean login(MemberRequest memberRequest) throws Exception;
+    public boolean checkUserIdValidation(String userId) throws Exception;
+    /*
+    public List<Member> list() throws Exception;
+     */
+
+    public Optional<Member> findByAuth(Long memberNo);
+}
